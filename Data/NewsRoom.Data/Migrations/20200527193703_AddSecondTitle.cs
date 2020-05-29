6556b0e3945155b9b8658022c6e91c4e7050196c
LlubomirKiosev <kiosev.lubomir@abv.bg>
2020-05-30 01:00:40 +0300

    Add Second Title for News Add Parent Comment

---
+++ Data/NewsRoom.Data/Migrations/20200527193703_AddSecondTitle.cs
@@ -0,0 +1,37 @@
+﻿namespace NewsRoom.Data.Migrations
+{
+    using Microsoft.EntityFrameworkCore.Migrations;
+
+    public partial class AddSecondTitle : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SecondTitle",
+                table: "News",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "NewsId",
+                table: "Comments",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SecondTitle",
+                table: "News");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "NewsId",
+                table: "Comments",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int));
+        }
+    }
+}
---
+++ Data/NewsRoom.Data/Migrations/20200527194224_AddSecondTitleWithLastastNFCore.cs
@@ -0,0 +1,15 @@
+﻿namespace NewsRoom.Data.Migrations
+{
+    using Microsoft.EntityFrameworkCore.Migrations;
+
+    public partial class AddSecondTitleWithLastastNFCore : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
